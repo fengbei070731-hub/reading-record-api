4ad01854a24621e446d1b36c84f90c25f2d554e6
GFBei <fengbei070731@gmail.com>
2026-08-01 16:09:09 +0800

    init: create reading-record-api with first endpoint and test

---
+++ src/main/java/com/example/readingrecordapi/ReadingRecordApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.readingrecordapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingRecordApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadingRecordApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/readingrecordapi/ReadingRecordApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.readingrecordapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingRecordApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
